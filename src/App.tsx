5c3cd95bcfdb5026d4cee64c8b39564bac2a9ade
PlannoVitorDev <planno.vitor@gmail.com>
2026-08-28 14:02:59 -0300

    feat: add global updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
 import Header from './components/header/header'
+import Home from './pages/home/home'
+import SobreMim from './pages/sobre-mim/sobre-mim'
 
 function App() {
   return (
     <>
       <Header />
+
+      <Home />
+
+      <SobreMim />
     </>
   )
 }
